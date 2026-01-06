f992ab43cc978dcf599ed41d453bc01e24a27668
Charity Rymbai <charityrymbai@hotmail.com>
2026-01-06 20:28:47 +0000

    added prisma and JWT errors in error handling middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,4 +1,6 @@
+import { Prisma } from "@prisma/client";
 import type { NextFunction, Request, Response } from "express";
+import { JsonWebTokenError, NotBeforeError, TokenExpiredError } from "jsonwebtoken";
 import { ZodError } from "zod";
 
 export function errorMiddleware(
@@ -15,14 +17,34 @@ export function errorMiddleware(
       message: error.message
     })); 
 
-    res.status(400).json({
+    return res.status(400).json({
       status: 'error', 
       type: 'ZodValidationError', 
       errors: formattedErrors
     })
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === 'P2002') {
+      console.log('ERROR LOGGING: ', err, 'ERROR ENDED'); 
+      if (err.meta?.target?.includes("email")) {
+        return res.status(409).json({
+          status: 'error', 
+          error_type: "Conflict",
+          message: "Email already registered"
+        }); 
+      }
+    }
+  } else if (err instanceof TokenExpiredError ||
+    err instanceof JsonWebTokenError ||
+    err instanceof NotBeforeError
+   ) {
+    return res.status(401).json({
+      status: 'error', 
+      type: err.name, 
+      message: err.message
+    })
   }
   
-  res.status(500).json({
+  return res.status(500).json({
     status: 'error',
     message: 'Internal Server Error',
   })
