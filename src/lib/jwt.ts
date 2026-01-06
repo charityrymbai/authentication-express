4b045c7f41801ff7709035ca1d9302dc8656ba27
Charity Rymbai <charityrymbai@hotmail.com>
2026-01-06 20:27:35 +0000

    added verifyJWTToken fn and corrected some env vars names

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,12 +1,12 @@
-import jwt from 'jsonwebtoken'; 
+import jwt, { type JwtPayload } from 'jsonwebtoken'; 
 
 const jwtSecret = process.env.JWT_SECRET;
 if (!jwtSecret) {
   throw new Error("JWT_SECRET is not set");
 }
 
-const accessTokenTTL: number = parseInt(process.env.ACCESS_TOKEN_TTL_IN_MS?? '') || 5*60; // 5mins  
-const refreshTokenTTL: number = parseInt(process.env.REFRESH_TOKEN_TTL_IN_MS?? '') || 7*24*60*60;  //7days
+const accessTokenTTL: number = parseInt(process.env.ACCESS_TOKEN_TTL_IN_SECS?? '') || 5*60; // 5mins  
+const refreshTokenTTL: number = parseInt(process.env.REFRESH_TOKEN_TTL_IN_SECS?? '') || 7*24*60*60;  //7days
 
 export const generateJWTToken = (data: any, expiresInMs: number) => {
   return jwt.sign({
@@ -26,3 +26,12 @@ export const generateRefreshToken = (
     userId, jti
   }, refreshTokenTTL); 
 }
+
+interface RefreshTokenPayload extends jwt.JwtPayload {
+  userId: string;
+  jti: string; 
+}
+
+export const verifyJWTToken = (token: string): RefreshTokenPayload => {
+  return jwt.verify(token, jwtSecret) as RefreshTokenPayload;
+};
