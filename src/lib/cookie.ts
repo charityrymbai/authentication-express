f6de477f7da74bf538c0c7b1746fc89efedf0f9d
Charity Rymbai <charityrymbai@hotmail.com>
2026-01-08 17:49:21 +0000

    added expiry according to token type

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -3,14 +3,18 @@ import type { Response } from "express";
 const cookieOnSecureConnection =
   process.env.COOKIE_ON_SECURE_CONNECTION === "true";
 
+const refreshTokenTTL: number = parseInt(process.env.REFRESH_TOKEN_TTL_IN_SECS?? '') || 7*24*60*60;  //7days
+const accessTokenTTL: number = parseInt(process.env.ACCESS_TOKEN_TTL_IN_SECS?? '') || 5*60;  //5mins
+
 export const setTokenAsCookie = (
   res:Response, 
-  tokenName: string, 
+  tokenName: 'refresh-token' | 'access-token', 
   token: string
 ) => {
   res.cookie(tokenName, token, {
     sameSite: 'lax', 
     secure: cookieOnSecureConnection, 
-    httpOnly: true
+    httpOnly: true,
+    maxAge: (tokenName === 'refresh-token')? refreshTokenTTL*1000 : accessTokenTTL*1000  
   })
 } 
